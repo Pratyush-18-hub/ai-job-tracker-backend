cbf35cf9502317495176c07fd9c7672e50870cec
Pratyush <pratyushsahoo230@gmail.com>
2026-07-01 19:09:50 +0530

    Fix CORS for Vercel frontend

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://ai-job-tracker-xi.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
