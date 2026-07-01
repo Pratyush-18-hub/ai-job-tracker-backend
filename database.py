df885a09582a12b1a21cbfbe8e3289da092679a2
Pratyush <pratyushsahoo230@gmail.com>
2026-07-01 16:55:46 +0530

    Connect backend to Supabase

--- database.py
+++ database.py
@@ -1,8 +1,24 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-from sqlalchemy.orm import sessionmaker,declarative_base
+# Load variables from .env
+load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:Pratyush%401234@localhost:5433/jobtracker"
+# Read DATABASE_URL from .env
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Create database connection
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind = engine)
+
+# Create database session
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+# Base class for SQLAlchemy models
 Base = declarative_base()
